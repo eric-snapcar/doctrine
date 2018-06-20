1cecc07013883dd0026bfed9838cfb497a181332
Eric Hth <eric@snapcar.com>
2018-06-21 00:28:48 +0200

    RootController SearchController render() ...

--- src/controller/RootController.js
+++ src/controller/RootController.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../style.css';
 import HomeController from './HomeController';
+import SearchController from './SearchController';
 export default class RootController extends Component {
   render() {
     return (
       <div className="rootController">
-          <HomeController />
+          <SearchController />
       </div>
     );
   }
---
+++ src/controller/SearchController.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../style.css';
+
+export default class SearchController extends Component {
+  componentDidMount(){
+    this.searchBar.focus();
+  }
+  onSearch(text){
+    console.log("onSearch");
+    console.log(text);
+  }
+  render() {
+    return (
+      <div className="searchController">
+          <img className="searchLogo"  src="logo.svg"   onClick={this.props.logOut}  alt="" />
+          <input onChange={(event) => this.onSearch(event.target.value)} ref = { element => this.searchBar = element} type="text" className="searchBar" placeholder={this.props.placeholder ? this.props.placeholder : "Search"} autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"/>
+      </div>
+    );
+  }
+}
