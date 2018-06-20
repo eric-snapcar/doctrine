21a36e224d6b3d6611baf6e3fa85859b89c65870
Eric Hth <eric@snapcar.com>
2018-06-20 13:35:03 +0200

    RootController.js renamed

--- src/RootController.js
+++ src/RootController.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
-class App extends Component {
+export default class RootController extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="RootController">
           TEST
       </div>
     );
   }
 }
 
-export default App;
+ 
