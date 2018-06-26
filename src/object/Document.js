2d12a409e7997ccabd77dfc8b16c4cf2bbceb380
Eric Hth <eric@snapcar.com>
2018-06-27 00:13:24 +0200

    Document this.title this.authors

--- src/object/Document.js
+++ src/object/Document.js
@@ -2,9 +2,8 @@
 export default class Document {
   constructor(data,searchText){
       if ( data ) {
-        console.log("Document");
-        console.log(data);
-
+        this.title = data.title;
+        this.author_name = data.author_name;
       }
   }
 }
