bc8c35cb6e62eab7d655ff33380e13889137ca53
Eric Hth <eric@snapcar.com>
2018-07-03 10:07:25 +0200

    Document this.cover_id this.key

--- src/object/Document.js
+++ src/object/Document.js
@@ -1,9 +1,19 @@
 
 export default class Document {
   constructor(data,searchText){
+    console.log(data);
       if ( data ) {
         this.title = data.title;
         this.author_name = data.author_name;
+        this.key = data.key;
+        this.cover_i = data.cover_i;
       }
+      console.log(this);
+  }
+  imageUrl(){
+
+  }
+  targetUrl(){
+
   }
 }
