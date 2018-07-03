8ee36e1c92f68a5d4001b05a0df8f00debcabe52
Eric Hth <eric@snapcar.com>
2018-07-03 10:09:44 +0200

    Document this.key this.cover_id imageUrl() targetUrl() ...

--- src/object/Document.js
+++ src/object/Document.js
@@ -1,19 +1,17 @@
 
 export default class Document {
   constructor(data,searchText){
-    console.log(data);
       if ( data ) {
         this.title = data.title;
         this.author_name = data.author_name;
         this.key = data.key;
         this.cover_i = data.cover_i;
       }
-      console.log(this);
   }
   imageUrl(){
-
+    return "http://covers.openlibrary.org/b/id/"+this.cover_i+".jpg";
   }
   targetUrl(){
-
+    return "http://covers.openlibrary.org/books/"+this.key;
   }
 }
