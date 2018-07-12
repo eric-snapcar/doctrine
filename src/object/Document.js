8ead2accce1f5fa48616e8672883691c5b89bf8e
Eric hth <eric@macbook-pro-de-eric.home>
2018-07-13 00:48:16 +0200

    Document title() details() description() ...

--- src/object/Document.js
+++ src/object/Document.js
@@ -2,11 +2,22 @@
 export default class Document {
   constructor(data,searchText){
       if ( data ) {
+        console.log("Document");
+        console.log(data);
         this.title = data.title;
         this.author_name = data.author_name;
         this.key = data.key;
         this.cover_i = data.cover_i;
       }
+  }
+  title(){
+    return this.title;
+  }
+  details(){
+    return "By " + this.author_name;
+  }
+  description(){
+    return "Description";
   }
   imageUrl(){
     if(this.cover_i != null){
